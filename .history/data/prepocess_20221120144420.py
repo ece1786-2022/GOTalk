19d601d17720f648b0c2cb1bc44dfb7201d25575
Darren Shao <yezhengshao@Yezhengs-MacBook-Air.local>
2022-11-20 17:31:03 -0500

    prepocess complete

--- .history/data/prepocess_20221120144420.py
+++
@@ -1,4 +0,0 @@
-file = open("./txtfiles/got1.txt", "r")
-for i in range(20):
-    file.readline()
-    print(file.readline())
--- data/prepocess.py
+++ data/prepocess.py
@@ -1,4 +1,6 @@
 from nltk.tokenize import word_tokenize
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+
 file = open("./txtfiles/got1_Jon.txt", "r")
 testfile = open("Jon_1.txt","w")
 
@@ -7,11 +9,11 @@ testfile = open("Jon_1.txt","w")
 # Jon index 0, "" => token, 
 # Jon mid, mid -> front, 1 and 2 combine
 # Jon mid, no 2, find next period. second " before Jon 
-START = "<START>"
-END = "<END>"
+START = "<BOS>"
+END = "<EOS>"
 
 
-verbs = ["said", "felt"]
+verbs = ["said", "felt", "told"]
 def alterline(line:str): 
     words = word_tokenize(line)
     # case 2
@@ -23,7 +25,13 @@ def alterline(line:str):
         if word == '”':
             end.append(i)
     if len(start) > 2 or len(start) != len(end): 
-        return line 
+        return line.rstrip()
+    if words[0] == 'Jon':
+        if len(start) == 1:
+            word_ls = list(map(lambda x: x.replace('“', '<START>'), words))
+            word_ls = list(map(lambda x: x.replace('”', '<END>'), word_ls))
+            return TreebankWordDetokenizer().detokenize(word_ls)
+            # print('\n')
     if len(start) == 2: 
         pos = end[0]
         if words[pos+1] == "Jon" and (words[pos+2][-2:] == "ed" or words[pos+2] in verbs):
@@ -31,8 +39,7 @@ def alterline(line:str):
             dialogue_1 = [START] + words[start[0] + 1:end[0]]
             dialogue_2 = words[start[1]+1:end[1]] + [END]
             newline = words[:start[0]] + mid_context + dialogue_1 + dialogue_2 + words[end[1]+1:]
-            # print(line)
-            # print(newline)
+            return TreebankWordDetokenizer().detokenize(newline)
     
     # case 3
     if len(start) == 1:
@@ -42,20 +49,20 @@ def alterline(line:str):
             context = words[end[0]+1:]
             dialogue = [START] + words[start[0] + 1:end[0]] + [END]
             newline = words[:start[0]] + context + dialogue
-            print(line)
             print(newline)
-
-
-        # prev = word
-
+            return TreebankWordDetokenizer().detokenize(newline)
+    return line.rstrip()
 tmp = []
 for i in range(2205):
     line = file.readline()
     if line != "\n":
-        alterline(line)
+        newline = alterline(line)
+        testfile.write(newline)
+        testfile.write('\n')
+testfile.close()
 
-#         testfile.write(line)
-# testfile.close()
+
+
 # print(tmp, tmp.index(6))
 
 # s = '''“He’s not like the others,” Jon said. “He never makes a sound. That’s why I named him Ghost. That, and because he’s white. The others are all dark, grey or black.” '''
