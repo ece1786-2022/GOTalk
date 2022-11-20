58fc43b527d522268edd34b5a7d199a2620fbd1a
huangtuoyue <huangtuoyue@gmail.com>
2022-11-20 16:01:07 -0500

    update data clean for CASE1

---
+++ .history/data/prepocess_20221120135834.py
@@ -0,0 +1,4 @@
+file = open("./txtfiles/got1.txt", "r")
+for i in range(20):
+    file.readline()
+    print(file.readline())
---
+++ .history/data/prepocess_20221120141245.py
@@ -0,0 +1,10 @@
+file = open("./txtfiles/got1.txt", "r")
+for i in range(20):
+    file.readline()
+    print(file.readline())
+    
+# text = ''
+# with open("./txtfiles/got1.txt", 'rt') as file_in:
+#     for line in file_in:
+#         text = text + line
+# print(text[:100])
