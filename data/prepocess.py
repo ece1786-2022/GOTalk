7eda32e98bdc4f91d9041a1a9a073e3f3340c4ff
Darren Shao <yezhengshao@Yezhengs-MacBook-Air.local>
2022-11-20 14:39:30 -0500

    find Jon section

--- data/prepocess.py
+++ data/prepocess.py
@@ -1,4 +1,22 @@
 file = open("./txtfiles/got1.txt", "r")
-for i in range(20):
-    file.readline()
-    print(file.readline())
+testfile = open("Jon_1.txt","w")
+
+
+# cases: clean symbol near ""
+# Jon index 0, "" => token, 
+# Jon mid, mid -> front, 1 and 2 combine
+# Jon mid, no 2, find next period. second " before Jon 
+
+verbs = ["said", "felt"]
+def alterline(line:str): 
+    words = line.split()
+    
+
+
+
+for i in range(980):
+    line = file.readline()
+    if line != "\n" and i >= 817:
+        print(line)
+        testfile.write(line)
+testfile.close()
